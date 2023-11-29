abefefe3c47e01801a54b187683f263225a7e2c3
tranthetan2101 <tantran21012000@gmail.com>
2023-11-29 14:49:33 +0700

    config env and remove sanctum

---
+++ api/app/Exceptions/NotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Enums\StatusCode;
+use Throwable;
+
+class NotFoundException extends AbstractException
+{
+    public function __construct(string $message = "Not found", int $statusCode = 422, ?Throwable $previous = null)
+    {
+        $this->message = $message;
+        $this->statusCode = $statusCode;
+        parent::__construct($message, $statusCode, $previous);
+    }
+}
---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ResponseTrait;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    use ResponseTrait;
+    public function createUser(): JsonResponse
+    {
+
+    }
+    public function updateUser(): JsonResponse
+    {
+
+    }
+    public function getUser(): JsonResponse
+    {
+
+    }
+    public function getUsers(): JsonResponse
+    {
+
+    }
+}
